eeb0dc44da646f22be49d42b53c9ca0dca7faafa
mister-ben <1676039+mister-ben@users.noreply.github.com>
2025-10-08 16:10:18 +0200

    fix braces

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -20,51 +20,7 @@ module.exports = function(config) {
           'browserstack.video': false,
         }
       }
-    // browserstackLaunchers(defaults) {
-    //   return {
-    //     bsAndroid: {
-    //       base: 'BrowserStack',
-    //       browser: 'chrome',
-    //       os: 'Android',
-    //       os_version: '16',
-    //       'browserstack.local': 'false',
-    //       'browserstack.video': 'false'
-    //     },
-    //     bsSafari: {
-    //       base: 'BrowserStack',
-    //       browser: 'safari',
-    //       os: 'OS X',
-    //       os_version: 'Tahoe',
-    //       'browserstack.local': 'false',
-    //       'browserstack.video': 'false'
-    //     },
-    //     bsIPhone: {
-    //       base: 'BrowserStack',
-    //       device: 'iPhone 15',
-    //       os: 'ios',
-    //       os_version: '26',
-    //       'browserstack.local': 'false',
-    //       'browserstack.video': 'false'
-    //     },
-    //     bsChromeDefault: defaults.bsChrome,
-    //     bsChrome: {
-    //       base: 'BrowserStack',
-    //       browser: 'chrome',
-    //       os: 'Windows',
-    //       os_version: '10',
-    //       'browserstack.local': 'false',
-    //       'browserstack.video': 'false'
-    //     },
-    //     bsFirefox: {
-    //       base: 'BrowserStack',
-    //       browser: 'firefox',
-    //       os: 'Windows',
-    //       os_version: '11',
-    //       'browserstack.local': 'false',
-    //       'browserstack.video': 'false'
-    //     }
-    //   };
-    // }
+    }
   };
 
   config = generate(config, options);
