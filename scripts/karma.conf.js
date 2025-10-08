1d2faa1bbf7be591c1d89d722eca828888bdf27c
mister-ben <1676039+mister-ben@users.noreply.github.com>
2025-10-08 15:48:12 +0200

    browserstack wit more launchers

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -4,38 +4,58 @@ module.exports = function(config) {
 
   // see https://github.com/videojs/videojs-generate-karma-config
   // for options
-  const options = {};
+  const options = {
+    browserstackLaunchers(defaults) {
+      /* eslint-disable quote-props, camelcase */
+      return {
+        bsAndroid: {
+          base: 'BrowserStack',
+          browser: 'chrome',
+          os: 'Android',
+          os_version: '16',
+          'browserstack.local': 'false',
+          'browserstack.video': 'false'
+        },
+        bsSafari: {
+          base: 'BrowserStack',
+          browser: 'safari',
+          os: 'OS X',
+          os_version: 'Tahoe',
+          'browserstack.local': 'false',
+          'browserstack.video': 'false'
+        },
+        bsIPhone: {
+          base: 'BrowserStack',
+          device: 'iPhone 15',
+          os: 'ios',
+          os_version: '26',
+          'browserstack.local': 'false',
+          'browserstack.video': 'false'
+        },
+        bsChrome: {
+          base: 'BrowserStack',
+          browser: 'chrome',
+          os: 'Windows',
+          os_version: '10',
+          'browserstack.local': 'false',
+          'browserstack.video': 'false'
+        },
+        bsFirefox: {
+          base: 'BrowserStack',
+          browser: 'firefox',
+          os: 'Windows',
+          os_version: '11',
+          'browserstack.local': 'false',
+          'browserstack.video': 'false'
+        }
+      };
+    }
+  };
 
   config = generate(config, options);
 
-  delete config.customLaunchers.bsSafari12;
-  delete config.customLaunchers.bsSafari14;
-
-  /* eslint-disable quote-props, camelcase */
-  config.customLaunchers.bsAndroid = {
-    base: 'BrowserStack',
-    browser: 'chrome',
-    os: 'Android',
-    os_version: '16',
-    'browserstack.local': 'false',
-    'browserstack.video': 'false'
-  };
-  config.customLaunchers.bsIPhone = {
-    base: 'BrowserStack',
-    device: 'iPhone 15',
-    os: 'ios',
-    os_version: '26',
-    'browserstack.local': 'false',
-    'browserstack.video': 'false'
-  };
-  config.customLaunchers.bsSafari = {
-    base: 'BrowserStack',
-    browser: 'safari',
-    os: 'OS X',
-    os_version: 'Tahoe',
-    'browserstack.local': 'false',
-    'browserstack.video': 'false'
-  };
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(config, null, 2));
 
   // any other custom stuff not supported by options here!
 };
