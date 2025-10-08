2a9c604751ff8f69230db21f44cae15901c45995
mister-ben <1676039+mister-ben@users.noreply.github.com>
2025-10-08 15:21:24 +0200

    browserstack

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -8,5 +8,34 @@ module.exports = function(config) {
 
   config = generate(config, options);
 
+  delete config.customLaunchers.bsSafari12;
+  delete config.customLaunchers.bsSafari14;
+
+  /* eslint-disable quote-props, camelcase */
+  config.customLaunchers.bsAndroid = {
+    base: 'BrowserStack',
+    browser: 'chrome',
+    os: 'Android',
+    os_version: '16',
+    'browserstack.local': 'false',
+    'browserstack.video': 'false'
+  };
+  config.customLaunchers.bsIPhone = {
+    base: 'BrowserStack',
+    device: 'iPhone 15',
+    os: 'ios',
+    os_version: '26',
+    'browserstack.local': 'false',
+    'browserstack.video': 'false'
+  };
+  config.customLaunchers.bsSafari = {
+    base: 'BrowserStack',
+    browser: 'safari',
+    os: 'OS X',
+    os_version: 'Tahoe',
+    'browserstack.local': 'false',
+    'browserstack.video': 'false'
+  };
+
   // any other custom stuff not supported by options here!
 };
