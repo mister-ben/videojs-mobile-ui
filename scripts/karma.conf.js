f8964a59c715328c7a06c7ed94175b8bd4b508fc
mister-ben <1676039+mister-ben@users.noreply.github.com>
2025-10-08 15:58:15 +0200

    browserstack defaults

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -5,51 +5,52 @@ module.exports = function(config) {
   // see https://github.com/videojs/videojs-generate-karma-config
   // for options
   const options = {
-    browserstackLaunchers(defaults) {
-      /* eslint-disable quote-props, camelcase */
-      return {
-        bsAndroid: {
-          base: 'BrowserStack',
-          browser: 'chrome',
-          os: 'Android',
-          os_version: '16',
-          'browserstack.local': 'false',
-          'browserstack.video': 'false'
-        },
-        bsSafari: {
-          base: 'BrowserStack',
-          browser: 'safari',
-          os: 'OS X',
-          os_version: 'Tahoe',
-          'browserstack.local': 'false',
-          'browserstack.video': 'false'
-        },
-        bsIPhone: {
-          base: 'BrowserStack',
-          device: 'iPhone 15',
-          os: 'ios',
-          os_version: '26',
-          'browserstack.local': 'false',
-          'browserstack.video': 'false'
-        },
-        bsChrome: {
-          base: 'BrowserStack',
-          browser: 'chrome',
-          os: 'Windows',
-          os_version: '10',
-          'browserstack.local': 'false',
-          'browserstack.video': 'false'
-        },
-        bsFirefox: {
-          base: 'BrowserStack',
-          browser: 'firefox',
-          os: 'Windows',
-          os_version: '11',
-          'browserstack.local': 'false',
-          'browserstack.video': 'false'
-        }
-      };
-    }
+    // browserstackLaunchers(defaults) {
+    //   /* eslint-disable quote-props, camelcase */
+    //   return {
+    //     bsAndroid: {
+    //       base: 'BrowserStack',
+    //       browser: 'chrome',
+    //       os: 'Android',
+    //       os_version: '16',
+    //       'browserstack.local': 'false',
+    //       'browserstack.video': 'false'
+    //     },
+    //     bsSafari: {
+    //       base: 'BrowserStack',
+    //       browser: 'safari',
+    //       os: 'OS X',
+    //       os_version: 'Tahoe',
+    //       'browserstack.local': 'false',
+    //       'browserstack.video': 'false'
+    //     },
+    //     bsIPhone: {
+    //       base: 'BrowserStack',
+    //       device: 'iPhone 15',
+    //       os: 'ios',
+    //       os_version: '26',
+    //       'browserstack.local': 'false',
+    //       'browserstack.video': 'false'
+    //     },
+    //     bsChromeDefault: defaults.bsChrome,
+    //     bsChrome: {
+    //       base: 'BrowserStack',
+    //       browser: 'chrome',
+    //       os: 'Windows',
+    //       os_version: '10',
+    //       'browserstack.local': 'false',
+    //       'browserstack.video': 'false'
+    //     },
+    //     bsFirefox: {
+    //       base: 'BrowserStack',
+    //       browser: 'firefox',
+    //       os: 'Windows',
+    //       os_version: '11',
+    //       'browserstack.local': 'false',
+    //       'browserstack.video': 'false'
+    //     }
+    //   };
+    // }
   };
 
   config = generate(config, options);
